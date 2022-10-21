b334753ec5cf8eec03bf541d687d83b28c6accf2
rbusche <rbusche@posteo.net>
2022-10-21 15:52:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Reproduction of https://github.com/kubernetes-client/javascript/issues/864
+
+Run 
+
+    npm run build
+
+to reproduce the error.
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import k8s from "@kubernetes/client-node";
+
+
+const main = () => {
+    console.log('Hello World!');
+    console.log(k8s.APIS)
+}
